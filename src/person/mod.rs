168724b604aab83681a2477448c439003a0b5ee3
theloosygoose <g.ramahenry2439@gmail.com>
2023-06-13 09:38:23 -0400

    weights for different single values

--- src/person/mod.rs
+++ src/person/mod.rs
@@ -27,29 +27,54 @@ impl Person {
 
         match value_type {
             OffVal::Initiator => {
-                let value = ((rtngs.ball_handle as f32 * OFF_ABILITY[0])
-                         + (rtngs.off_awareness as f32 * OFF_ABILITY[1])
-                         + (rtngs.touch as f32 * OFF_ABILITY[2])
-                         + (rtngs.speed as f32 * OFF_ABILITY[2])
-                         + (rtngs.burst as f32 * OFF_ABILITY[3])
-                         + (personality.intelligence as f32 * OFF_ABILITY[4])
-                         + (personality.dog as f32 * OFF_ABILITY[5]))
-                    / (7.0);
+                let value = ((rtngs.ball_handle as f32 * INITIATOR[0])
+                             + (rtngs.off_awareness as f32 * INITIATOR[1])
+                             + (rtngs.pass_accuracy as f32 * INITIATOR[2])
+                             + (rtngs.touch as f32 * INITIATOR[3])
+                             + (rtngs.shot_form as f32 * INITIATOR[4])
+                             + (rtngs.speed as f32 * INITIATOR[5])
+                             + (rtngs.burst as f32 * INITIATOR[6])
+                             + (personality.intelligence as f32 * INITIATOR[7])
+                             + (personality.creativity as f32 * INITIATOR[8]))
+                    / INITIATOR.len() as f32;
 
-                OffPreview(self.person_id.clone(), value, value_type)
+                return OffPreview(self.person_id.clone(), value, value_type);
             },
             OffVal::OffBall => {
+                let value = ((rtngs.off_awareness as f32 * OFF_BALL[0])
+                             + (rtngs.speed as f32 * OFF_BALL[1])
+                             + (rtngs.burst as f32 * OFF_BALL[2])
+                             + (personality.intelligence as f32 * OFF_BALL[3])
+                             + (personality.dog as f32 * OFF_BALL[4]))
+                    / OFF_BALL.len() as f32;
 
+                return OffPreview(self.person_id.clone(), value, value_type);
             },
             OffVal::Driving => {
+                let value = ((rtngs.off_awareness as f32 * DRIVING[0])
+                             + (rtngs.touch as f32 * DRIVING[1])
+                             + (rtngs.speed as f32 * DRIVING[2])
+                             + (rtngs.burst as f32 * DRIVING[3])
+                             + (rtngs.strength as f32 * DRIVING[4])
+                             + (rtngs.fluidity as f32 * DRIVING[5])
+                             + (personality.intelligence as f32 * DRIVING[6])
+                             + (personality.dog as f32 * DRIVING[7]))
+                    / DRIVING.len() as f32;
 
+                return OffPreview(self.person_id.clone(), value, value_type);
             },
             OffVal::FloorSpacing => {
+                let value = ((rtngs.off_awareness as f32 * SPACING[0])
+                             + (rtngs.shot_form as f32 * SPACING[1])
+                             + (rtngs.touch as f32 * SPACING[2])
+                             + (personality.intelligence as f32 * SPACING[3]))
+                    / SPACING.len() as f32;
 
+
+                return OffPreview(self.person_id.clone(), value, value_type);
             },
-
-
         }
+    }
 
     pub fn def_preview(&self, value_type: DefVal) -> DefPreview {
         let rtngs = self.intangibles;
@@ -57,16 +82,31 @@ impl Person {
 
         match value_type {
             DefVal::OffBall => {
+                let value = ((rtngs.def_awareness as f32 * D_OFFBALL[0])
+                             + (rtngs.lateral as f32 * D_OFFBALL[1])
+                             + (rtngs.speed as f32 * D_OFFBALL[2])
+                             + (rtngs.burst as f32 * D_OFFBALL[3])
+                             + (rtngs.height as f32 * D_OFFBALL[4])
+                             + (rtngs.wingspan as f32 * D_OFFBALL[5])
+                             + (personality.dog as f32 * D_OFFBALL[6])
+                             + (personality.intelligence as f32 * D_OFFBALL[7]))
+                    / D_OFFBALL.len() as f32;
 
+                return DefPreview(self.person_id.clone(), value, value_type);
             },
             DefVal::OnBall => {
+                let value = ((rtngs.def_awareness as f32 * D_ONBALL[0])
+                             + (rtngs.lateral as f32 * D_ONBALL[1])
+                             + (rtngs.fluidity as f32 * D_ONBALL[2])
+                             + (rtngs.burst as f32 * D_ONBALL[3])
+                             + (rtngs.height as f32 * D_ONBALL[4])
+                             + (rtngs.wingspan as f32 * D_ONBALL[5])
+                             + (personality.dog as f32 * D_ONBALL[6])
+                             + (personality.intelligence as f32 * D_ONBALL[7]))
+                    / D_ONBALL.len() as f32;
 
-            },
-
-
+                return DefPreview(self.person_id.clone(), value, value_type);
+            }
         }
-
-    }
-
     }
 }
--- src/weights.rs
+++ src/weights.rs
@@ -1,18 +1,59 @@
-pub const OFF_ABILITY: [f32; 6] = [
+pub const INITIATOR: [f32; 9] = [
     1.5, //ball handle
-    1.2, //off_awareness
-    1.0, //touch & speed
-    1.1, //burst
+    1.8, //off_awareness
+    1.4, //pass_accuracy
+    1.1, //touch 
+    1.0, //shot_form
+    1.0, //speed
+    0.9, //burst
     1.6, //intelligence
-    1.1, //dog
+    1.1, //creativity
 ];
 
-pub const DEF_ABILITY: [f32; 7] = [
-    1.5, //lateral
-    1.0, //def_awareness
-    1.2, //speed
-    1.3, //strength
-    1.1, //burst
-    1.0, //intelligence
+pub const OFF_BALL: [f32; 5] = [
+    1.5, //off_awareness
+    1.5, //speed
+    1.3, //burst
+    1.4, //intelligence
     1.3, //dog
+];
+
+pub const DRIVING: [f32; 8] = [
+    1.0, //off_awareness
+    1.0, //touch
+    1.3, //speed
+    1.7, //burst
+    1.4, //strength
+    1.3, //fluidity
+    0.9, //intelligence
+    1.4, //dog
+];
+
+pub const SPACING: [f32; 4] = [
+    1.0, //off_awareness
+    1.7, //shot_form
+    1.4, //touch
+    1.0, //intelligence
+];
+
+pub const D_ONBALL: [f32; 8] = [
+    1.0, //def_awareness
+    1.7, //lateral
+    1.4, //fluidity
+    1.1, //burst
+    1.0, //height
+    1.0, //wingspan
+    1.7, //dog
+    1.0, //intelligence
+];
+
+pub const D_OFFBALL: [f32; 8] = [
+    1.7, //def_awareness
+    1.0, //lateral
+    1.2, //speed
+    1.1, //burst
+    1.2, //height
+    1.4, //wingspan
+    1.3, //dog
+    1.0, //intelligence
 ];
